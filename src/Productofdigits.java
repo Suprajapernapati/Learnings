30f44fa24c6f083d7bbae9b3d8d06d6612e190b2
Supraja <suprajapernapati@gmail.com>
2026-06-13 20:45:14 +0530

    Product of digits

---
+++ src/Productofdigits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Productofdigits {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        int product = 1;
+        while(num>0){
+            int digit = num%10;
+            product = product * digit;
+            num = num/10;
+        }
+        System.out.println(product);
+        sc.close();
+    }
+}
+
