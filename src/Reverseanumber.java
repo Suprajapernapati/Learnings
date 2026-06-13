c2a0795b17b3ad59b6d99aeae3bcc1ced02dca61
Supraja <suprajapernapati@gmail.com>
2026-06-13 11:45:41 +0530

    Added reverse number program

---
+++ src/Reverseanumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Reverseanumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num =sc.nextInt();
+        int rem;
+        int reverse = 0;
+        while(num>0){
+            rem = num%10;
+            reverse = reverse*10+rem;
+            num = num/10;
+        }
+        System.out.println(reverse);
+        sc.close();
+    }
+
+}
